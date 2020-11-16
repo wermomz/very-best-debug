193362606fe578529451ab7ff276593e035d7a22
litalorlev1323 <lorlev@chicagobooth.edu>
2020-11-16 03:26:57 +0000

    done

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -28,7 +28,7 @@ class VenuesController < ApplicationController
   def update
     the_id = params.fetch("the_id")
 
-    venue = Venue.where({ :id => the_id })
+    venue = Venue.where({ :id => the_id }).first
     venue.address = params.fetch("query_address")
     venue.name = params.fetch("query_name")
     venue.neighborhood = params.fetch("query_neighborhood")
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get("/venues", { :controller => "venues", :action => "index" })
   get("/insert_venue_record", { :controller => "venues", :action => "create" })
   get("/venues/:an_id", { :controller => "venues", :action => "show" })
-  post("/update_venue/:the_id", { :controller => "application", :action => "update" })
+  get("/update_venue/:the_id", { :controller => "venues", :action => "update" })
   get("/delete_venue/:id_to_delete", { :controller => "venues", :action => "destroy" })
   
   get("/insert_comment_record", { :controller => "comments", :action => "create" })
